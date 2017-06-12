29617aea9ddfeede7cd41d35279021c0e30caff5
jbastias <jorgebastias@hotmail.com>
2017-06-12 13:03:57 -0600

    test github with jenkins

--- karma.conf.js
+++ karma.conf.js
@@ -40,6 +40,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
-    singleRun: true
+    singleRun: true,
+    debug: true
   });
 };
